b8c8019373794a56fba66f13e90fbe75183fda21
Sara Khaled <sakh1132000@gmail.com>
2026-04-26 22:29:04 +0300

    1st schemas.py by sara

---
+++ backend/app/auth/schemas.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel, EmailStr
+from typing import Optional
+
+class UserCreate(BaseModel):
+    username: str
+    email: EmailStr
+    password: str
+
+class UserLogin(BaseModel):
+    username: str
+    password: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    username: Optional[str] = None
